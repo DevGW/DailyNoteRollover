fa8bf7f935de7da5ce6401613f437c9c78ec2034
DevGW <jason@cryptechstudios.com>
2023-10-27 09:29:56 -0500

    update

--- main.ts
+++ main.ts
@@ -1,134 +1,84 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, TFile } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class DailyNoteRollover extends Plugin {
+	app: App;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+	constructor(app: App, pluginId: string) {
+		super(app, pluginId);
+		this.app = app;
+	}
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	async onload(): Promise<void> {
+		console.log('DailyNoteRollover plugin loaded.');
+		this.app.workspace.on('layout-ready', () => this.initialize());
+	}
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	async initialize(): Promise<void> {
+		console.log('DailyNoteRollover plugin initialized.');
 
-	async onload() {
-		await this.loadSettings();
+		// Debug
+		console.log('Initializing rollover');
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		const dailyNotesPlugin = this.app.plugins.getPlugin('daily-notes');
+		let dateFormat: string = 'YYYY-MM-DD';
+		if (dailyNotesPlugin) {
+			dateFormat = dailyNotesPlugin.options.format;
+		}
+		const yesterday: string = window.moment().subtract(1, 'days').format(dateFormat);
+		const today: string = window.moment().format(dateFormat);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		let todayNote: TFile | null = this.app.vault.getAbstractFileByPath(`Daily/${today}.md`);
+		if (todayNote) {
+			console.log("Today's note already exists. Skipping rollover.");
+			return;
+		}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		let yesterNote: TFile | null = this.app.vault.getAbstractFileByPath(`Daily/${yesterday}.md`);
+		console.log(`Yesterday's note exists: ${yesterNote ? 'Yes' : 'No'}`);
+
+		// Debug
+		console.log('yesterNote', yesterNote);
+
+		let yesterContent: string = yesterNote ? await this.app.vault.read(yesterNote) : '';
+
+		// Debug
+		console.log('yesterContent', yesterContent);
+
+		const lines: string[] = yesterContent.split('\n');
+		let filteredContent: string = '';
+		let skip = false;
+		let skipLevel = 0; // New variable to keep track of indents to skip
+
+		for (const line of lines) {
+			const indentLevel = line.search(/\S/); // Gets the indent level by finding the index of the first non-whitespace character
+
+			if (line.startsWith("- [x]")) {
+				skip = true;
+				skipLevel = indentLevel;
+			} else if (indentLevel <= skipLevel) {
+				skip = false;
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+
+			if (!skip) {
+				filteredContent += line + '\n';
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		// Debug
+		console.log('filteredContent', filteredContent);
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// let todayNote: TFile | null = this.app.vault.getAbstractFileByPath(`Daily/${today}.md`);
+		if (!todayNote) {
+			await this.app.vault.create(`Daily/${today}.md`, '');
+			todayNote = this.app.vault.getAbstractFileByPath(`Daily/${today}.md`);
+		}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// Debug
+		console.log('todayNote', todayNote);
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		if (todayNote) {
+			await this.app.vault.modify(todayNote, filteredContent);
+		}
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
 }
